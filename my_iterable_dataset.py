21484d63015e640fa3c631fcab395104fc57271d
alsa0217 <aleksandr.savinov@netcracker.com>
2020-09-30 00:47:26 +0400

    Iterable dataset now uses prescribed path to load data

--- my_iterable_dataset.py
+++ my_iterable_dataset.py
@@ -3,38 +3,37 @@ import random
 import os, glob
 from scipy.io import wavfile
 
-voices = []
-for file in glob.glob("./data/voice/*.wav"):
-    samplerate, data = wavfile.read(file)
-    voices.append(torch.from_numpy(data).float() / 32768)
-noises = []
-for file in glob.glob("./data/noise/*.wav"):
-    _, data = wavfile.read(file)
-    noises.append(torch.from_numpy(data).float() / 32768)
-    
-train_sample_length = 22528 #int(samplerate * 2)
-silence = torch.zeros(train_sample_length)
-
 
 class MyIterableDataset(torch.utils.data.IterableDataset):
     TYPE_VOICE = 1
     TYPE_VOICE_NOISE = 2
     TYPE_NOISE = 3
     
-    def __init__(self, data_len, ):
+    def __init__(self, data_len, base_dir, train_sample_length = 22528):
         super().__init__()
         self.data_len = data_len
         
+        self.voices = []
+        for file in glob.glob(os.path.join(base_dir, "voice", "*.wav")):
+            samplerate, data = wavfile.read(file)
+            self.voices.append(torch.from_numpy(data).float() / 32768)
+        self.noises = []
+        for file in glob.glob(os.path.join(base_dir, "noise", "*.wav")):
+            _, data = wavfile.read(file)
+            self.noises.append(torch.from_numpy(data).float() / 32768)
+
+        self.train_sample_length = train_sample_length
+        self.silence = torch.zeros(train_sample_length)
 
     def _voice_sample(self):
-        s = voices[random.randint(0, len(voices) - 1)]
-        offset = random.randint(0, len(s) - train_sample_length - 1)
-        return s[offset:offset + train_sample_length]
+        s = self.voices[random.randint(0, len(self.voices) - 1)]
+        offset = random.randint(0, len(s) - self.train_sample_length - 1)
+        return s[offset:offset + self.train_sample_length]
     
     def _noise_sample(self):
-        s = noises[random.randint(0, len(noises) - 1)]
-        offset = random.randint(0, len(s) - train_sample_length - 1)
-        return s[offset:offset + train_sample_length]
+        s = self.noises[random.randint(0, len(self.noises) - 1)]
+        offset = random.randint(0, len(s) - self.train_sample_length - 1)
+        return s[offset:offset + self.train_sample_length]
         
     def generate_with_noise(self):
         voice = self._voice_sample()
@@ -47,7 +46,7 @@ class MyIterableDataset(torch.utils.data.IterableDataset):
     
     def generate_noise(self):
         noise = self._noise_sample()
-        return {'input': noise / random.uniform(6, 14), 'target': silence, 'type': self.TYPE_NOISE}
+        return {'input': noise / random.uniform(6, 14), 'target': self.silence, 'type': self.TYPE_NOISE}
     
     def generate(self):
         r = random.uniform(0, 1)
